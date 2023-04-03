ec0fe2428e570fd8361fc28d3c7b98ff2a9c7b5f
Ivan Hrytsenko <ivanhrytsenko23143@gmail.com>
2023-04-03 15:17:38 +0300

    fix jest test bug

--- client/config/jest/cssTransform.js
+++
@@ -1,8 +0,0 @@
-const sass = require("node-sass");
-
-module.exports = {
-  process(src, filename) {
-    const result = sass.renderSync({ file: filename });
-    return result.css.toString();
-  },
-};
